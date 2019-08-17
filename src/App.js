188c8376ecc1d1baed51e48217a996cf4fff4e61
Marcel Assis <marcel@marcelassis.com>
2019-08-17 09:46:40 -0300

    Hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 const App = () => {
-  return React.createElement('div', { id: 'something-important' }, [
-    React.createElement('h1', {}, 'Adopt-me!'),
-    React.createElement(Pet, {
-      name: 'Kika',
-      animal: 'dog',
-      breed: 'mix'
-    }),
-    React.createElement(Pet, {
-      name: 'Keka',
-      animal: 'dog',
-      breed: 'mix'
-    }),
-    React.createElement(Pet, {
-      name: 'Rufus',
-      animal: 'dog',
-      breed: 'Lhasa-Apsu'
-    })
-  ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt-me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById('root'));
+render(<App />, document.getElementById('root'));
